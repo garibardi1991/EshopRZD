2effb6eae1e38a7cd41f00c503d2f9546fa2f910
Igor pol <garibardi@mail.ru>
2023-03-12 15:06:04 +0300

    Add code

--- src/test/java/ru/eshoprzd/tests/TestsEshopRzd.java
+++ src/test/java/ru/eshoprzd/tests/TestsEshopRzd.java
@@ -1,16 +1,25 @@
 package ru.eshoprzd.tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.eshoprzd.config.Property;
 import ru.eshoprzd.helpers.Attach;
 
+import java.io.File;
+import java.util.stream.Stream;
+
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -81,22 +90,64 @@ public class TestsEshopRzd {
 
     @Test
     @Feature("Проверка сайта eshoprzd.ru")
-    @Story("Проверяем наличие раздела 'Об Электронном магазине'")
+    @Story("Проверяем наличие плашки с информацией 'На нашем сайте мы используем...'")
     @Owner("trubikhoviv")
     @Severity(SeverityLevel.BLOCKER)
     @Link(value = "Testing", url = "https://eshoprzd.ru/home")
-    @DisplayName("На главной странице должен отображаться раздел 'Об Электронном магазине'")
+    @DisplayName("На главной странице должна отображаться информация 'На нашем сайте мы используем файлы cookie. Продо...'")
     void titleTest() {
         step("Открываем сайт eshoprzd.ru", () ->
                 open("https://eshoprzd.ru/home"));
 
-        step("На главной странице должен отображаться раздел с наименование 'Об Электронном магазине'", () -> {
-            String expectedTitle = "Об Электронном магазине";
-            String actualTitle = title();
+        step("Проверка наличия отображения информации 'На нашем сайте мы используем файлы cookie. Продолжая работу на сайте, Вы даете согласие на использование файлов cookie.'", () ->
+            $(".ng-scope").shouldHave(text("На нашем сайте мы используем файлы cookie. Продолжая работу на сайте, Вы даете согласие на использование файлов cookie.")));
+    }
 
-            assertThat(actualTitle).isEqualTo(expectedTitle);
+
+    static Stream<Arguments> openEshopRzdCheckForm(){
+        File file = new File("src/test/resources/files/2.Хаски.jpg");
+        return Stream.of(
+                Arguments.of("Прочие вопросы", "Игорь", "garibardi@list.ru", "Hello", file, "Заполните все обязательные поля." ),
+                Arguments.of("Техническая поддержка, изменение реквизитов и пр.", "Вася", "garibardi@mail.ru", "Good job",file, "Заполните все обязательные поля.")
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    @Feature("Проверка сайта eshoprzd.ru")
+    @Story("Проверяем раздел 'Контакты'")
+    @Owner("trubikhoviv")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Testing", url = "https://eshoprzd.ru/home")
+    @DisplayName("Проверка заполнения обязательных полей и подгрузка файлов")
+    void openEshopRzdCheckForm(String subject, String name, String email, String text, File file, String message){
+        step("Открываем сайт eshoprzd.ru, раздел 'Контакты'", () ->
+        open("https://eshoprzd.ru/contacts"));
+
+        step("Заполянем 'Выберите тему'", () ->{
+        $("#theme").click();
+        $(byText(subject)).click();
+                });
+        step("Заполянем 'Пожалуйста, представьтесь'", () ->
+        $("#name").setValue(name));
+
+        step("Заполянем 'Электронная почта'", () ->
+        $("#email").setValue(email));
+
+        step("Заполянем поле 'Текст обращения'", () ->
+        $("#text").setValue(text));
+
+        step("Загружаем файл в раздел 'Документы'", () ->{
+        $("[ng-click='it.showAttachBlock()']").click();
+        $(".attachments__file input[type='file']").uploadFile(file);
+        });
+
+        step("Проверяем выходит ли сообщение 'Заполните все обязательные поля.'", () ->{
+        $("[ng-click='sendFeedback(content)']").click();
+        $(".alert-danger").shouldHave(Condition.text(message));
         });
     }
+
 
 }
 
